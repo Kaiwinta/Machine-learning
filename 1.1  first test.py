0268f7a770a514c6f8ebaa4c1dce19ce05b286b9
Kaiwinta <pradouratitouan@proton.me>
2023-06-21 14:01:15 +0200

    Update 1.1  first test.py

--- 1.1  first test.py
+++ 1.1  first test.py
@@ -2,5 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib as plt
 
-cois = ("fLength", "fWidth", "fSize", "fConc" , "fConc1" , "fAsym", "fM3Long", "fM3Trans" , "fAlpha" , "fDist" , "class")
-pd.read_csv("magic04.data")
+cols = ("fLength", "fWidth", "fSize", "fConc" , "fConc1" , "fAsym", "fM3Long", "fM3Trans" , "fAlpha" , "fDist" , "class")
+df =pd.read_csv("magic04.data", names=cols)
+print(df)
