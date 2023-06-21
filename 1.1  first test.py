3976b87dd4f729518bfc76c243204554eeaaed4a
Kaiwinta <pradouratitouan@proton.me>
2023-06-21 22:48:21 +0200

    Update 1.1  first test.py

--- 1.1  first test.py
+++ 1.1  first test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 #Le dataset importer parle de rayon lumineux d'un télescope
 #Les class sont g pour gamma et f pour l'autre
@@ -50,12 +50,17 @@ Different type of machine learning :
 """
 
 """
-    We divide thye datasets into 3 parts
+    We divide the datasets into 3 parts
 
     The training datasets
     The validation dataset
     The testing dataset
 
+"""
+"""
+    L1 Loss:    loss = sum(abs(y_real - y_predicted))
+    L2 Loss:    loss = sum((y_real - y_predicted)**2)
+    Binary Cross entropy Loss  ==> un bordel
 """
 
 #L'on ajoute le nom des colonnes à chaque colonne
@@ -64,3 +69,15 @@ df =pd.read_csv("magic04.data", names=cols)
 
 #class est soit g ou f mais du coup on transformme en binaire 
 df["class"] = (df["class"] == "g").astype(int)
+#print(df.head())
+
+for label in cols[:-1]:
+    plt.hist(df[df['class']==1][label], color = 'blue', label = 'gamma', alpha = 0.7 , density = True)
+    plt.hist(df[df['class']==0][label], color = 'red', label = 'hadron', alpha = 0.7 , density = True)
+    plt.title(label)
+    plt.ylabel("Probability")
+    plt.xlabel(label)
+    plt.legend()
+    #plt.show()
+
+train , valid , test = np.split(df.sample(frac=1), [int(0.6* len(df)), int(0.8* len(df))])
