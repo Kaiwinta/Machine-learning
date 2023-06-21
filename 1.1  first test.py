9b1e951e3088f437745d4c7008a732f0c2af8cbc
Kaiwinta <pradouratitouan@proton.me>
2023-06-21 13:58:36 +0200

    first import

---
+++ 1.1  first test.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+cois = ("fLength", "fWidth", "fSize", "fConc" , "fConc1" , "fAsym", "fM3Long", "fM3Trans" , "fAlpha" , "fDist" , "class")
+pd.read_csv("magic04.data")
