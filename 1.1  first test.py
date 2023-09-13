7edb62949b4aea7e48acce404adf1e9461caa868
Kaiwinta <pradouratitouan@proton.me>
2023-09-13 17:56:02 +0200

    adding plot

--- 1.1  first test.py
+++ 1.1  first test.py
@@ -210,6 +210,24 @@ def svm_modelisation():
 
 import tensorflow as tf
 
+def plot_loss(history):
+    plt.plot(history.history['loss'],label = 'loss')
+    plt.plot(history.history['val_loss'],label = 'val_loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Binary crossentropy')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def plot_accuracy(history):
+    plt.plot(history.history['accuracy'],label = 'accuracy')
+    plt.plot(history.history['val_accuracy'],label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
 nn_model = tf.keras.Sequential([
     tf.keras.layers.Dense(32,activation='relu',input_shape = (10,)),
     tf.keras.layers.Dense(32,activation='relu'),
